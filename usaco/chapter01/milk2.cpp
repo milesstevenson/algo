b6687719bc7e792eb910a2cd1ee327b178a6c6f0
Miles Stevenson <miles.d.stevenson@gmail.com>
2015-08-05 19:36:29 -0500

    completed milking cows

--- usaco/chapter01/milk2.cpp
+++ usaco/chapter01/milk2.cpp
@@ -12,22 +12,9 @@ using namespace std;
 
 vector<pair<int,int> > times;
 
-bool disjointMilking(int current,  int finish) {
-  bool premise1 = times[current].first > finish;
-  bool premise2 = times[current].second > finish;
-  return premise1 && premise2;
-}
-
-bool extendMilking(int current, int finish) {
-  bool premise1 = times[current].second > finish;
-  bool premise2 = times[current].first <= finish;
-  return premise1 && premise2;
-}
-
-bool subsetMilking(int current, int finish) {
-  bool premise1 = times[current].second < finish;
-  bool premise2 = times[current].first < finish;
-  return premise1 && premise2;
+bool intersectMilking(int current, int finish) {
+  bool premise1 = times[current].first <= finish;
+  return premise1;
 }
 
 int main() {
@@ -45,27 +32,20 @@ int main() {
   int off, on, offmax = 0, onmax = finish - start;
   
   for (int i = 1; i < n; i++) {
-    // check if there is spare time between (i-1).finish and i.start
-    // this allows us to extend maximum down time
-    if (disjointMilking(i, finish)) {
-      off = times[i].first - finish;
-      offmax = max(offmax, off);
-      start = times[i].first;
-      finish = times[i].second;
-      onmax = max(onmax, finish-start);
-    }
-    // check if the intersection between current milking and past
-    // milking allows us to extend maximum milking time
-    else if (extendMilking(i, finish)) {
-      finish = times[i].second;
-      onmax = finish - start;
-    }
-    // inception (subset) milking... doesn't matter
-    // do nothing
-    else if (subsetMilking(i, finish))
-      continue;
+     if (intersectMilking(i, finish)) {
+       finish = max(finish, times[i].second);
+       start = min(start, times[i].first);
+       onmax = max(onmax, finish - start);
+     }
+     else {
+       off = times[i].first - finish;
+       offmax = max(offmax, off);
+       start = times[i].first;
+       finish = times[i].second;
+       onmax = max(onmax, finish-start);
+     }
   }
- 
+  
   fout << onmax << " " << offmax << endl;
   return 0;
 }
